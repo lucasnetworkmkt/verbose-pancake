cb93a95e73e93a37a5100bbd0e3c83e890c14987
lucasnetworkmkt <moveissv@gmail.com>
2026-02-16 14:04:30 -0300

    Update supabase.ts

--- services/supabase.ts
+++ services/supabase.ts
@@ -1,10 +1,11 @@
+
 import { createClient } from '@supabase/supabase-js';
 
 // Credenciais de Fallback (Garante que o app funcione mesmo se as variáveis de ambiente falharem)
 const DEFAULT_URL = "https://ulhcfwfoewdviirhupts.supabase.co";
 const DEFAULT_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVsaGNmd2ZvZXdkdmlpcmh1cHRzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzAwNjE1NzAsImV4cCI6MjA4NTYzNzU3MH0.WBRcoTPvb-rHCb5IYo72pyAoqL2qPZT-vuxOdj4p5So";
 
-let supabaseUrl = DEFAULT_URL;
+export let supabaseUrl = DEFAULT_URL;
 let supabaseKey = DEFAULT_KEY;
 
 try {

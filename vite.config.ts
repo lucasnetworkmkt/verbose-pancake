ba0c0cc6a2786c94cf14b46b3cd4a5edbd727bd6
lucasnetworkmkt <moveissv@gmail.com>
2026-02-18 22:04:39 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,18 @@
-
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
-  // Carrega variáveis de ambiente do diretório atual.
-  const env = loadEnv(mode, process.cwd(), '');
+  const env = loadEnv(mode, (process as any).cwd(), '');
 
   return {
     plugins: [react()],
+    define: {
+      // Maneira correta e segura de expor a API Key no Vite para a lib do Google
+      'process.env.API_KEY': JSON.stringify(env.API_KEY),
+    },
     server: {
       host: true
-    },
-    define: {
-      // Define process.env como um objeto contendo a API_KEY.
-      // Isso é mais seguro do que definir process.env.API_KEY e depois process.env = {}
-      'process.env': JSON.stringify({
-        API_KEY: env.API_KEY,
-        NODE_ENV: mode
-      })
-    },
-    build: {
-      chunkSizeWarningLimit: 1000
     }
   };
 });

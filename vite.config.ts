63879d67313e302fbf289488e85f0eb034a493c0
lucasnetworkmkt <moveissv@gmail.com>
2026-02-22 18:38:16 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,20 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: true
-  },
-  define: {
-    // Alguns pacotes dependem de process.env, isso evita que o build quebre
-    'process.env': {}
-  },
-  build: {
-    // Aumenta o limite de aviso de tamanho de chunk para evitar warnings desnecessários
-    chunkSizeWarningLimit: 1000
-  }
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    plugins: [react()],
+    server: {
+      host: true
+    },
+    define: {
+      'process.env.API_KEY': JSON.stringify(env.API_KEY)
+    },
+    build: {
+      // Aumenta o limite de aviso de tamanho de chunk para evitar warnings desnecessários
+      chunkSizeWarningLimit: 1000
+    }
+  };
 });
